ff95095ae8af16f071bf641b14d36c1a9536562d
Juan Antonio <jamll.muro@gmail.com>
2024-04-04 11:34:52 +0200

    Add a new function that returns the match date in a specific format

---
+++ src/utils/getMatchDate.js
@@ -0,0 +1,10 @@
+export const getMatchDate = ({ matchDate }) => {
+  const date = new Date(matchDate);
+
+  const weekDay = date.toLocaleString('en-US', { weekday: 'short'});
+  const numberDay = date.getDate();
+  const month = date.toLocaleString('en-US', { month: 'short'});
+  const matchHour = date.toLocaleString('en-US', { hour: '2-digit'})
+
+  return `${weekDay} ${numberDay}, ${month} - ${matchHour}`;
+};
