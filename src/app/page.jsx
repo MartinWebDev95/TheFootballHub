2a5b7c9b5f17d9c6dbf38e17686a2342e7dbc05b
Juan Antonio <jamll.muro@gmail.com>
2024-04-05 11:51:17 +0200

    Add to new components to the next fixtures in champions league section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,52 +1,37 @@
-import Match from "@/components/Match";
+import Carousel from "@/components/Carousel";
 import StandingTablePreview from "@/components/StandingTablePreview";
 import { getLaLigaStandingsMockup, getNextGamesChampionsMockup, getPremierLeagueStandingsMockup } from "@/lib/getData";
-
-export const CircleIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-circle lg:group-hover:fill-white">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-    </svg>
-  );
-}
 
 export default async function Home() {
   
   const standingsLaLiga = await getLaLigaStandingsMockup();
   const standingsPremierLeague = await getPremierLeagueStandingsMockup();
   const nextGamesChampions = await getNextGamesChampionsMockup();
-  
+
   return (
     <main className="w-full bg-green-950 overflow-x-hidden">
-      <div className="container mx-auto grid place-items-center">
-        <section className="grid grid-cols-2 auto-rows-min gap-8 py-8 items-start w-3/4">
-          <StandingTablePreview 
-            standing={standingsLaLiga.standings[0].filter((item) => item.rank <= 10)}
-            league={standingsLaLiga} 
-          />
+      <div className="container mx-auto grid place-items-center px-2 lg:px-0">
+        <section className="grid grid-cols-1 auto-rows-min md:grid-cols-2 gap-y-8 md:gap-8 py-8 items-start lg:w-3/4">
+          <div className='bg-sky-500 p-3 rounded-lg shadow-lg row-start-1 row-end-2 md:col-start-1 col-end-2 flex flex-col items-end h-full'>
+            <StandingTablePreview 
+              standing={standingsLaLiga.standings[0].filter((item) => item.rank <= 10)}
+              league={standingsLaLiga} 
+            />
+          </div>
 
-          <StandingTablePreview 
-            standing={standingsPremierLeague.standings[0].filter((item) => item.rank <= 10)} 
-            league={standingsPremierLeague}
-          />
+          <div className='bg-sky-500 p-3 rounded-lg shadow-lg row-start-2 row-end-3 md:row-start-1 md:row-end-2 md:col-start-2 md:col-end-3 flex flex-col items-end h-full'>
+            <StandingTablePreview 
+              standing={standingsPremierLeague.standings[0].filter((item) => item.rank <= 10)} 
+              league={standingsPremierLeague}
+            />
+          </div>
 
-          <div className="col-start-1 col-end-3 bg-[url('/assets/champions-banner.webp')] bg-no-repeat bg-cover bg-center rounded-lg shadow-lg overflow-x-hidden w-full text-white">
-            <h2 className="bg-black/60 text-balance text-center pt-4">Next Champions League Games</h2>
+          <div className="col-start-1 col-end-3 row-start-3 row-end-4 md:row-start-2 md:row-end-3 bg-[url('/assets/champions-banner.webp')] bg-no-repeat bg-cover bg-center rounded-lg shadow-lg overflow-x-hidden w-full text-white">
+            <h2 className="bg-black/60 text-balance text-center pt-4">
+              Upcomings Champions League Games
+            </h2>
 
-            <div className="overflow-x-hidden flex items-center gap-2 w-full text-white bg-black/60 py-4">
-              {nextGamesChampions.map((match) => (
-                <Match key={match.fixture.id} match={match} />
-              ))}
-            </div>
-            
-            <div className="flex items-center justify-center gap-2 pb-4 bg-black/60">
-              {nextGamesChampions.map((match) => (
-                <button type="button" key={match.fixture.id} className="group transition-all ease-in-out duration-300">
-                  <CircleIcon />
-                </button>
-              ))}
-            </div>
+            <Carousel matches={nextGamesChampions} />
           </div>
         </section>
       </div>
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,37 @@
+'use client'
+import { useRef } from 'react';
+import Match from './Match'
+import CarouselButton from './CarouselButton';
+
+const Carousel = ({ matches }) => {
+  const carouselRef = useRef();
+  const matchWidth = useRef();
+
+  return (
+    <>
+      <div ref={carouselRef} className="flex items-center w-full text-white">
+        {matches.map((match, index) => (
+          <Match 
+            key={match.fixture.id} 
+            match={match} 
+            matchRef={matchWidth} 
+            idMatch={index + 1} 
+          />
+        ))}
+      </div>
+      
+      <div className="flex items-center justify-center gap-2 pb-4 bg-black/60">
+        {matches.map((match, index) => (
+          <CarouselButton 
+            key={match.fixture.id}
+            carouselRef={carouselRef} 
+            matchWidth={matchWidth}
+            idButton={index} 
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Carousel
