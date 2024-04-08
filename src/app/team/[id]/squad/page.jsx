06ae3ca3d927d9f9abd44302a719afd7feff49cb
Juan Antonio <jamll.muro@gmail.com>
2024-04-08 09:46:38 +0200

    Add all the players of a specific team in the squad page

--- src/app/team/[id]/squad/page.jsx
+++ src/app/team/[id]/squad/page.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import { getTeamSquad } from '@/lib/getData';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const metadata = {
   title: "The Football Hub | Teams | Squad",
 };
 
-const SquadPage = ({ params }) => {
+async function SquadPage({ params }){
   const { id } = params;
 
+  const squadTeam = await getTeamSquad({ idTeam: id });
+
+  //Group the football squad array by the players positions
+  const squadByPositions = Object.groupBy(squadTeam, (player) => player.position)
+
   return (
-    <div>SquadPage</div>
+    <section className='container mx-auto px-2 lg:px-0 py-8'>
+      <ul>
+        {Object.entries(squadByPositions).map((items) => (
+          <li key={items[0]} className='group'>
+            <span className='group-first-of-type:mt-0 my-4 block font-semibold text-xl text-white'>
+              {items[0]}
+            </span>
+
+            <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              {items[1].map((player) => (
+                <li key={player.id}>
+                  <Link href={`/player/${player.id}`} className='flex items-center justify-between gap-4 bg-black text-white rounded-md p-3 shadow-xl'> 
+                    <div className='flex items-center gap-4'>                      
+                      <Image 
+                        src={player.photo} 
+                        width={40} 
+                        height={40} 
+                        alt={player.name} 
+                        className='rounded-full'
+                      />
+            
+                      <p className='truncate'>
+                        <span className='block text-sm'>{player.position}</span>
+                        <span>
+                          {player.name.length > 15 ? `${player.name.substring(0,15)}...` : player.name}
+                        </span>
+                      </p>
+                    </div>
+
+                    <p className='text-3xl font-semibold'>{player.number}</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
--- src/lib/getData.js
+++ src/lib/getData.js
@@ -2,6 +2,7 @@ import standingsLaLiga from '@/lib/mockupStandingLaLiga.json';
 import standingsPremier from '@/lib/mockupStandingPL.json';
 import nextGamesChampions from '@/lib/mockupNextGamesChampions.json';
 import infoTeam from '@/lib/mockupInfoTeam.json';
+import squadTeam from '@/lib/mockupSquad.json';
 
 export async function getLaLigaStandingsMockup(){
   return await standingsLaLiga.response[0].league;
@@ -17,4 +18,8 @@ export async function getNextGamesChampionsMockup(){
 
 export async function getInfoTeam({ idTeam }){
   return await infoTeam.response[0];
+}
+
+export async function getTeamSquad({ idTeam }){
+  return await squadTeam.response[0].players;
 }
