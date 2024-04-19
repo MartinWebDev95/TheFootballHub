ab7035dfa5129e5297ac8007769aebaa36540966
Juan Antonio <jamll.muro@gmail.com>
2024-04-19 14:10:58 +0200

    Change the Match component to show the result in case of penalties

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,4 +1,3 @@
-import { getMatchDate } from '@/utils/getMatchDate'
 import MatchTeam from './MatchTeam'
 
 const Match = ({ match, matchRef, idMatch }) => {
@@ -8,33 +7,31 @@ const Match = ({ match, matchRef, idMatch }) => {
 
       <div className="w-full">
         <p className="text-center mb-1 text-sm">{match.fixture.venue.name}</p>
-        {
-          match.goals.home === null ? (
-            <p className="text-center text-sm flex flex-col gap-1">
-              <span className='hidden md:block'>
-                {getMatchDate({ matchDate: match.fixture.date }).dateFull}
+        <div className="text-4xl flex justify-center items-center gap-2">
+          <p className={`${match.goals.home > match.goals.away ? 'font-bold' : 'font-normal'} flex items-center gap-2`}>
+            {match.score.penalty.home !== null && (
+              <span className={`${match.score.penalty.home > match.score.penalty.away ? 'font-bold' : 'font-normal'} text-sm`}>
+                ({match.score.penalty.home})
               </span>
-              <span className='md:hidden'>
-                {getMatchDate({ matchDate: match.fixture.date }).dateFull.split('-')[0]}
-              </span>              
-              <span className='md:hidden'>
-                {getMatchDate({ matchDate: match.fixture.date }).dateFull.split('-')[1]}
+            )}
+
+            {match.goals.home}
+          </p>
+
+          <p className="font-bold text-cyan-600">
+            -
+          </p>
+
+          <p className={`${match.goals.away > match.goals.home ? 'font-bold' : 'font-normal'} flex items-center gap-2`}>
+            {match.goals.away}
+
+            {match.score.penalty.away !== null && (
+              <span className={`${match.score.penalty.away > match.score.penalty.home ? 'font-bold' : 'font-normal'} text-sm`}>
+                ({match.score.penalty.away})
               </span>
-            </p>
-          ) : (
-            <p className="text-4xl flex justify-center items-center gap-2">
-              <span className={`${match.goals.home > match.goals.away ? 'font-bold' : 'font-normal'}`}>
-                {match.goals.home}
-              </span>
-              <span className="font-bold text-cyan-600">
-                -
-              </span>
-              <span className={`${match.goals.away > match.goals.home ? 'font-bold' : 'font-normal'}`}>
-                {match.goals.away}
-              </span> 
-            </p>
-          )
-        }
+            )}
+          </p> 
+        </div>
       </div>
 
       <MatchTeam team={match.teams.away} />
